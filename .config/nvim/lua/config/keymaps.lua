b88d7af47abd7908aaf8edc5b63a40c6a1bd6ce2
Bayarbileg Bayarsaikhan <bbayarbileg@gmail.com>
2025-03-16 00:52:40 +0100

    reinitiated configs to use lazyvim instead of lazy.nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,44 +1,17 @@
--- Keymaps are automatically loaded before lazy.nvim startup
+-- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- Normal mode mappings
+vim.keymap.set("n", "<C-h>", "4h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "4j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "4k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "4l", { noremap = true, silent = true })
 
--- Set leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Optional: Visual mode mappings
+vim.keymap.set("v", "<C-h>", "4h", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-j>", "4j", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-k>", "4k", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-l>", "4l", { noremap = true, silent = true })
 
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
--- Buffer navigation
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
--- Better indenting
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
-
--- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
-
--- Clear search highlights
-keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
-
--- Close buffer
-keymap("n", "<leader>c", "<cmd>bdelete<CR>", opts)
-
--- Save file
-keymap("n", "<leader>w", "<cmd>w<CR>", opts)
+-- create a simple ascii art
